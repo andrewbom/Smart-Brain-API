1cb83435cf4763e398c68dc1e1c87524ba8a8a7c
andrewbom <59633536+andrewbom@users.noreply.github.com>
2020-07-18 15:37:15 +1000

    security issue fixed

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,20 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '0a27447ffab34d4ea34c344296c9b325'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    // This part has been updated with the recent Clarifai changed. Used to be:
+    // .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
 const handleImage = (req, res, db) => {
 	const { id } = req.body;
   db('users').where('id', '=', id)
@@ -10,5 +27,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-  handleImage
+  handleImage,
+  handleApiCall
 }
